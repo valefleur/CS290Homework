1e27ee408803880e7711c44f3353663ca4087e59
Adina Edwards <edwardse@oregonstate.edu>
2016-01-18 16:29:07 -0800

    Added directory for Week3 activities and experiments.
    Started Activity1.js

---
+++ Week3/activity1.js
@@ -0,0 +1,7 @@
+console.log("Let's make sure JavaScript is working.");
+var name = "Adina"; // replaced string with my name
+console.log("The unicode chracters of your name are:") // No semi-colon?!
+for (var i =0; i < name.length; i++){
+    console.log(name.charCodeAt(i));
+}
+console.log("Copy and paste these values for activity credit.") // again, No semi-colon?
