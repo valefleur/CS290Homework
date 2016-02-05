c6df1a65de88e09600fd0cbae59192dd9f88b2cb
Adina Edwards <edwardse@oregonstate.edu>
2016-02-04 20:52:31 -0800

    Two ways of making tables
    Created two different ways of making tables in JS.  Each way uses
    mostly different methods, and one way uses a higher-order function
    (although it’s not terribly useful).

--- Week5/HW/script.js
+++ Week5/HW/script.js
@@ -1,3 +1,35 @@
+// to get practice, rewrite createRow or wrapper as a higher-order function
+function createPartOfTable(header, then) {
+    if (header) { //create THEADER & header row 
+        var table = document.getElementsByTagName("TABLE");
+        var head = document.createElement("THEAD");
+        head.border = "gold";
+        head.innerHTML = "higher-order table";
+        table[0].appendChild(head);
+        console.log("THEAD created");
+        //TODO: create a td in the head
+        //finally, create TBODY
+        var body = document.createElement("TBODY");
+        table[0].appendChild(body);
+        console.log("TBODY created");
+    }
+    else { then; }
+}
+
+var newTableRow = function(node, length) {
+    var row = node.insertRow(-1);
+    for (var i = 0; i < length; i++){
+        var cell = row.insertCell(i);
+        //enter cell content here
+        cell.innerHTML = "cell contents" + i;
+    }
+}
+
+var nothing = function(){
+    return true;
+}
+
+
 function createRow(parent, columns, header){
     // parent is the table node (thead or tbody)
     // columns is the number of cells to add
@@ -6,16 +38,33 @@ function createRow(parent, columns, header){
     //now fill new row with cells
     for (var i = 0; i < columns; i++){
         var cell;
-        if (header) { cell = document.createElement("TH"); }
-        else { cell = row.createElement("TD"); }
-        cell.innerHTML = "text before adding cell";
+        if (header) { 
+            cell = document.createElement("TH"); 
+            cell.innerHTML = "normal function table";
+        }
+        else { 
+            cell = row.createElement("TD");
+            cell.innerHTML = "text before adding cell" + i;
+        }
         row.appendChild(cell);
         console.log("created row i: " + i );
     }
     
 }
 
+
+console.log("DEBUG");
+//var location = document.getElementById("gameboard");
 var table = document.createElement("TABLE");
-table.border = "2"; //so I can see it
-console.log("Created table");
-createRow(table, 4, true);
+table.border = "2";
+document.body.appendChild(table);
+console.log("Table created");
+createPartOfTable(true, nothing);
+createPartOfTable(false, newTableRow(table, 4));
+console.log("DEBUG");
+
+var table1 = document.createElement("TABLE");
+document.body.appendChild(table1);
+table1.border = "2";//so I can see it
+console.log("Created table1");
+createRow(table1, 4, true);
