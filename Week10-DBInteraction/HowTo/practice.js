d49e4cf556cde75a4188f9b4cc481fd2b0c651c9
Adina Edwards <valefleur@gmail.com>
2016-03-06 15:47:04 -0800

    Started Framework & Contents of HowTo Website
    Set up most of practice framework (some was lost), beginnings of
    Handlebars framework, wrote up outline, wrote up documentation on
    everything except the requests themselves.

---
+++ Week10-DBInteraction/HowTo/practice.js
@@ -0,0 +1,31 @@
+//set up a function that makes a request to Echo Nest
+// and writes the response out to the console
+function makeRequest(){
+    var sync = true;
+    var sendString = null;
+    
+    //set up variables which hold request info
+    var url = "http://developer.echonest.com/api/v4/";
+    var getArtist = "artist/";
+    var hotttnesss = "hotttnesss";
+    var withkey = "?api_key=FILDTEOIK2HBORODV";
+    var ofArtist = "&name=lady+gaga";
+    
+    var artist = document.getElementById("artist").value;
+    document.getElementById("debug").innerHTML = "<br>Artist is: " + artist;
+    sendString = url + getArtist + hotttnesss + withkey + ofArtist;
+    console.log("**sendString is: " + sendString);
+    
+    //set up js requirements for making requests
+    var request = new XMLHttpRequest();
+    request.open("GET", sendString, sync);
+    console.log("**Opened connection");
+    request.addEventListener("load", function(){
+        console.log("**Hit a 'load' event");
+        var response = JSON.parse(request.responseText);
+        console.log(response);
+    });
+    request.send(null);
+    console.log("**Sent null");
+}
+ makeRequest();
